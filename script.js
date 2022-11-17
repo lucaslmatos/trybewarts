100fc2778fa160c5d4ba7dcaa4756af4241bfc65
Lucas Matos <lucaslmatos18@gmail.com>
2022-11-17 17:49:12 -0300

    Requisito 18 e 19

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const email = document.querySelector('#email');
 const senha = document.querySelector('#senha');
 const botaoEntrar = document.querySelector('#botaoEntrar');
+const concordar = document.querySelector('#agreement');
+const botaoSubmit = document.querySelector('#submit-btn');
 
 botaoEntrar.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+concordar.addEventListener('change', () => {
+  if (concordar.value === 'checked') {
+    botaoSubmit.disabled = true;
+  } else {
+    botaoSubmit.disabled = false;
   }
 });
