7c8cad8f2956d626aeca2ca5aa6ad57776fe4c28
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-11-17 17:59:04 -0300

    Botao submit corrigido

--- script.js
+++ script.js
@@ -13,9 +13,38 @@ botaoEntrar.addEventListener('click', () => {
 });
 
 concordar.addEventListener('change', () => {
-  if (concordar.value === 'checked') {
-    botaoSubmit.disabled = true;
-  } else {
-    botaoSubmit.disabled = false;
+  console.log(concordar.value);
+  if (concordar.value === 'on') {
+    botaoSubmit.toggleAttribute('disabled');
   }
 });
+
+// const page = document.getElementsByTagName('main')[0];
+// const buttonSubmit = document.getElementById('submit-btn');
+
+// function createForm() {
+//   const formData = document.createElement('div');
+//   formData.id = 'form-data';
+//   page.innerHTML = '';
+//   page.appendChild(formData);
+//   for (let i = 0; i < 4; i+= 1) {
+//     const phraseElement = document.createElement('p');
+//     formData.appendChild(phraseElement);
+//     const phrasesForm = formData.children;
+//   }
+// }
+
+// function saveInfos() {
+//   const nameUser = `${document.getElementById('input-name').value} ${document.getElementById('input-last-name')}`;
+//   const emailUser = document.getElementById('email').value;
+//   const homeUser = document.getElementById('house').value
+//   const savedInfo = [`Nome: ${nameUser}`, `Email: ${emailUser}`, `Casa: ${homeUser}`, `Família: ${}`, `Matérias: ${}`];
+
+// }
+
+// function buttonEvent() {
+//   createForm();
+//   saveInfos();
+// }
+
+// button.addEventListener('click', buttonEvent);
